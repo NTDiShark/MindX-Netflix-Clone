d8c23d1dad219f96eb88c4d7bfa720ad5103ba72
NTDiShark <datnguyen2298@gmail.com>
2023-07-03 21:45:17 +0700

    ...

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,41 +1,48 @@
-import React, {useState, useEffect} from 'react';
-import axios from '../../axios';
-import './Row.css'
+import React, { useState, useEffect } from "react";
+import axios from "../../axios";
+import "./Row.css";
 
-const base_url = 'https://image.tmdb.org/t/p/original'
+const base_url = "https://image.tmdb.org/t/p/original";
 
 const Row = (props) => {
-    const {title, fetchUrl, isLargeRow, getMovie} = props
+  const { title, fetchUrl, isLargeRow, getMovie } = props;
 
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        async function fetchData() {
-            const request = await axios.get(fetchUrl)
-            setMovies(request.data.results)
-        }
-        fetchData()
-    }, [fetchUrl])
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-    return (
-        <div className='row' >
-            <h2>{title}</h2>
-            <div className='row-poster'>
-
-                {movies.length >0 && movies.map(movie => (
-                    <div style={{position: 'relative'}} key={movie.id} onClick={() => getMovie(movie)}>
-                        <img 
-                            className={`poster ${isLargeRow && 'posterLarge'}`}
-                            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
-                            alt={movie.name}/>
-                        <div className='movie-title'>
-                            {movie?.title || movie?.name || movie?.original_name}
-                        </div>
-                    </div>
-                ))}
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row-poster">
+        {movies.length > 0 &&
+          movies.map((movie) => (
+            <div
+              style={{ position: "relative" }}
+              key={movie.id}
+              onClick={() => getMovie(movie)}
+            >
+              <img
+                className={`poster ${isLargeRow && "posterLarge"}`}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+              <div className="movie-title">
+                {movie?.title || movie?.name || movie?.original_name}
+              </div>
             </div>
-        </div>
-    );
-}
+          ))}
+      </div>
+    </div>
+  );
+};
 
-export default Row
+export default Row;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -3,17 +3,33 @@ import { useRecoilValue } from "recoil";
 import { valueSearchStore } from "../store/valueSearchStore";
 import { searchMovie } from "../servicess/functions";
 import { BASE_URL_IMAGE } from "../requests";
-import useScrollPosition from "../hooks/useScrollPosition";
+import { getMovieTrailerVsGenres } from "../servicess/functions";
+import Modal from "../components/Modal/Modal";
+import ReactPlayer from "react-player";
 
 const SearchScreen = () => {
   const valueSeach = useRecoilValue(valueSearchStore);
   const [movies, setMovies] = useState([]);
+  const [currentMovie, setCurrentMovie] = useState({});
+  const [showModal, setshowModal] = useState(false);
+  const [trailerKey, settrailerKey] = useState("");
 
-  const position = useScrollPosition();
-  console.log(
-    "🚀 ~ file: SearchScreen.js:13 ~ SearchScreen ~ position:",
-    position
-  );
+  const handleCloseModal = () => {
+    setshowModal(false);
+    setCurrentMovie({});
+  };
+
+  const handleClickMovie = (movie) => {
+    setCurrentMovie(movie);
+    setshowModal(true);
+  };
+
+  useEffect(() => {
+    if (currentMovie?.id)
+      getMovieTrailerVsGenres(currentMovie).then((res) =>
+        settrailerKey(res.trailerKey)
+      );
+  }, [currentMovie]);
 
   useEffect(() => {
     searchMovie(valueSeach).then((data) => {
@@ -29,7 +45,11 @@ const SearchScreen = () => {
         {movies &&
           movies?.length > 0 &&
           movies.map((movie) => (
-            <div style={{ position: "relative" }} key={movie.id}>
+            <div
+              style={{ position: "relative" }}
+              key={movie.id}
+              onClick={() => handleClickMovie(movie)}
+            >
               <img
                 className={`poster`}
                 src={`${BASE_URL_IMAGE}${
@@ -44,6 +64,15 @@ const SearchScreen = () => {
             </div>
           ))}
       </div>
+      <Modal visible={showModal} center onClose={handleCloseModal}>
+        <div className="popup-detail-movie">
+          <ReactPlayer
+            playing
+            controls
+            url={`https://www.youtube.com/watch?v=${trailerKey}`}
+          />
+        </div>
+      </Modal>
     </div>
   );
 };
